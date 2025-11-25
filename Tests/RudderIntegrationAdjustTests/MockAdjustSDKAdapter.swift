2ecd4aab8e36f6e3819b7f3da59dd8fe063b24e8
Satheesh Kannan <satheesh@rudderstack.com>
2025-11-25 17:57:56 +0530

    feat: add mock adjust sdk adapter

---
+++ Tests/RudderIntegrationAdjustTests/MockAdjustSDKAdapter.swift
@@ -0,0 +1,51 @@
+//
+//  MockAdjustSDKAdapter.swift
+//  RudderIntegrationAdjust
+//
+//  Created by Satheesh Kannan on 25/11/25.
+//
+
+import Testing
+import AdjustSdk
+import RudderStackAnalytics
+@testable import RudderIntegrationAdjust
+
+// MARK: - Mock Adapter
+final class MockAdjustSDKAdapter: AdjustSDKAdapter {
+    var initCalled = false
+    var initConfig: ADJConfig?
+
+    var trackCalled = false
+    var trackedEvent: ADJEvent?
+
+    var setPartnerParamsCalled = false
+    var partnerPayload: Event?
+
+    var removeGlobalPartnerParametersCalled = false
+    var onAttributionChanged: ((ADJAttribution) -> Void)?
+    
+    func initSDK(adjustConfig: ADJConfig?) {
+        initCalled = true
+        initConfig = adjustConfig
+    }
+
+    func track(event: ADJEvent) {
+        trackCalled = true
+        trackedEvent = event
+    }
+
+    func setPartnerParams(payload: Event) {
+        setPartnerParamsCalled = true
+        partnerPayload = payload
+    }
+
+    func removeGlobalPartnerParameters() {
+        removeGlobalPartnerParametersCalled = true
+    }
+}
+
+// MARK: - Callback Spy (for attribution callback)
+final class AttributionCallbackSpy {
+    var invoked = false
+    var received: ADJAttribution?
+}

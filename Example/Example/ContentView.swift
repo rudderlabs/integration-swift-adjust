cfe9500046ce68e5cd67b85236bab2ec98be2cf2
Satheesh Kannan <satheesh@rudderstack.com>
2025-11-26 15:24:54 +0530

    chore: update example app content view

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
+// MARK: - ContentView
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 16.0) {
+                Button("Identify Event") {
+                    AnalyticsManager.shared.identifyEvent()
+                }
+                .buttonStyle(PrimaryButtonStyle())
+                
+                Button("Track Event") {
+                    AnalyticsManager.shared.trackEvent()
+                }
+                .buttonStyle(PrimaryButtonStyle())
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Adjust Example")
         }
-        .padding()
+    }
+}
+
+// MARK: - PrimaryButtonStyle
+struct PrimaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .font(.title3)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
     }
 }
 

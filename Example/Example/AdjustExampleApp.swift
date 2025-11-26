69ce02bca526c286da787681f383104f709e6f37
Satheesh Kannan <satheesh@rudderstack.com>
2025-11-26 12:55:13 +0530

    chore: update adjust example app implementation

--- Example/Example/AdjustExampleApp.swift
+++ Example/Example/AdjustExampleApp.swift
@@ -6,12 +6,37 @@
 //
 
 import SwiftUI
+import RudderStackAnalytics
+import RudderIntegrationAdjust
 
+// MARK: - AdjustExampleApp
 @main
 struct AdjustExampleApp: App {
+    
+    init() {
+        self.setupAnalytics()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    func setupAnalytics() {
+        LoggerAnalytics.logLevel = .verbose
+        
+        // Configuration for RudderStack Analytics
+        let configuration = Configuration(writeKey: "", dataPlaneUrl: "")
+        
+        // Initialize Analytics
+        let analytics = Analytics(configuration: configuration)
+        
+        // Add Firebase Integration
+        let adjustIntegration = AdjustIntegration()
+        analytics.add(plugin: adjustIntegration)
+        
+        // Store analytics instance globally for access in ContentView
+        AnalyticsManager.shared.analytics = analytics
     }
 }

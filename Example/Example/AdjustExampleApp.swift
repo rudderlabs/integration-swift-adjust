c4c43014814f8ea2dee973aa9053a9021d421ab8
Satheesh Kannan <satheesh@rudderstack.com>
2025-11-26 11:38:06 +0530

    feat: add empty example app

---
+++ Example/Example/AdjustExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdjustExampleApp.swift
+//  Example
+//
+//  Created by Satheesh Kannan on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AdjustExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/ExampleTests/ExampleTests.swift
@@ -0,0 +1,12 @@
+//
+//  ExampleTests.swift
+//  ExampleTests
+//
+//  Created by Satheesh Kannan on 26/11/25.
+//
+
+import Testing
+
+struct ExampleTests {
+
+}
